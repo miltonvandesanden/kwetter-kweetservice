fbd8e368c14a3e50a67ea5203b36f38a8415edb8
Milton van de Sanden <m.vandesanden@student.fontys.nl>
2021-03-24 11:01:35 +0100

    init project based on start.spring.io

---
+++ src/main/java/fhict/kwetter/kweetservice/KweetserviceApplication.java
@@ -0,0 +1,13 @@
+package fhict.kwetter.kweetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KweetserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KweetserviceApplication.class, args);
+	}
+
+}
