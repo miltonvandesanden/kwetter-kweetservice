8d6ada02ee357e71b3cb2a20388b7c0cff9314f3
Milton van de Sanden <m.vandesanden@student.fontys.nl>
2021-06-15 16:13:57 +0200

    Revert "Revert "Update Config.java""
    This reverts commit a8f0ccac0742e715738fe8b1bbd83696e4f7ef4e.

--- src/main/java/fhict/kwetter/kweetservice/presentation/controller/Config.java
+++ src/main/java/fhict/kwetter/kweetservice/presentation/controller/Config.java
@@ -6,11 +6,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class Config implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-            .allowedOrigins();
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
